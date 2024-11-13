af6fe91de4d433e6bd6d53ac3301989cc90ca277
Diletant <al@naun.com>
2024-11-13 21:54:47 +0700

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,30 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+	public static final String NEW_VALUE = "";
+	public String surname;
+	String name;
+	private int sizeOfEmpty = 10;
+	
+	Broken() {
+		
+	}
+
+	void echo() {
+	}
+
+	void media(Object obj) {
+		if (obj != null) {
+			System.out.println(obj);
+		}
+	}
+
+	void methodPart1(int a, int b, int c, int d) {
+		
+	}
+	
+	void methodPart2(int e, int f, int g, int h) {
+		
+	}
+
+}
