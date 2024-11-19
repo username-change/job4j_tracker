21260c7a93ee055559450aed1c4d9077804be68c
Alex <differentplayer@yandex.ru>
2024-11-19 16:37:44 +0700

    8. Date. Отображение даты. [#310017 #522505]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,11 +5,7 @@ import java.time.LocalDateTime;
 public class Item {
 	private int id;
 	private String name;
-	private LocalDateTime created  = LocalDateTime.now();
-
-	public LocalDateTime getCreated() {
-		return created;
-	}
+	private LocalDateTime created = LocalDateTime.now();
 
 	public Item() {
 
@@ -22,6 +18,10 @@ public class Item {
 	public Item(int id, String name) {
 		this.id = id;
 		this.name = name;
+	}
+
+	public LocalDateTime getCreated() {
+		return created;
 	}
 
 	public int getId() {
