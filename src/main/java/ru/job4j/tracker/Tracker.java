c6b017c258e45963a44cf4658dc9bb8c81a6e733
Diletant <al@naun.com>
2024-11-25 17:23:04 +0700

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,25 +1,52 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
-    private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;
+	private final Item[] items = new Item[100];
+	private int ids = 1;
+	private int size = 0;
 
-    public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
-        return item;
-    }
+	public Item add(Item item) {
+		item.setId(ids++);
+		items[size++] = item;
+		return item;
+	}
 
-    public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
-    }
+	public Item findById(int id) {
+		Item rsl = null;
+		for (int index = 0; index < size; index++) {
+			Item item = items[index];
+			if (item.getId() == id) {
+				rsl = item;
+				break;
+			}
+		}
+		return rsl;
+	}
+
+	public Item[] findByName(String key) {
+		Item[] result = new Item[size];
+		int count = 0;
+		for (int index = 0; index < size; index++) {
+			Item item = items[index];
+			if (item.getName().equals(key)) {
+				result[count++] = item;
+			}
+		}
+		return Arrays.copyOf(items, count);
+
+	}
+
+	public Item[] findAll() {
+		Item[] result = new Item[size];
+		int count = 0;
+		for (int index = 0; index < size; index++) {
+			if (items[index] != null) {
+				result[count++] = items[index];
+			}
+		}
+		return Arrays.copyOf(items, count);
+	}
+
 }
