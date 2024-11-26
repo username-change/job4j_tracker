b6e4ee1922a47d27bc3dba615b8b29f3f70acfcf
Diletant <al@naun.com>
2024-11-26 23:39:58 +0700

    3.2. Реализация класса StartUI. Вывод всех заявок [#500745]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,42 +3,51 @@ package ru.job4j.tracker;
 import java.util.Scanner;
 
 public class StartUI {
-	
+
 	public void init(Scanner scanner, Tracker tracker) {
 		boolean run = true;
 		while (run) {
-		    showMenu();
+			showMenu();
 			System.out.print("Выбрать: ");
 			int select = Integer.parseInt(scanner.nextLine());
 			if (select == 0) {
-			    System.out.println("=== Создание новой заявки ===");
-			    System.out.print("Введите имя: ");
-			    String name = scanner.nextLine();
-			    Item item = new Item(name);
-			    tracker.add(item);
-			    System.out.println("Добавленная заявка: " + item);
+				System.out.println("=== Создание новой заявки ===");
+				System.out.print("Введите имя: ");
+				String name = scanner.nextLine();
+				Item item = new Item(name);
+				tracker.add(item);
+				System.out.println("Добавленная заявка: " + item);
+			} else if (select == 1) {
+				System.out.println("=== Вывод всех заявок ===");
+				Item[] items = tracker.findAll();
+				if (items.length > 0) {
+					for (Item item : items) {
+						System.out.println(item);
+					}
+				} else {
+					System.out.println("Хранилище еще не содержит заявок");
+				}
 			} else if (select == 6) {
 				run = false;
 			}
 		}
-		
-    }
+	}
 
-    private void showMenu() {
-    	String[] menu = {
-    	        "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
-    	        "Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
-    	        "Завершить программу"
-    	};
-    	System.out.println("Меню:");
-    	   for (int i = 0; i < menu.length; i++) {
-    	        System.out.println(i + ". " + menu[i]);
-    	   }
-    }
+	private void showMenu() {
+		String[] menu = {
+				"Добавить новую заявку", "Показать все заявки", "Изменить заявку",
+				"Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
+				"Завершить программу" 
+		};
+		System.out.println("Меню:");
+		for (int i = 0; i < menu.length; i++) {
+			System.out.println(i + ". " + menu[i]);
+		}
+	}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Tracker tracker = new Tracker();
-        new StartUI().init(scanner, tracker);
-    }
-} 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Tracker tracker = new Tracker();
+		new StartUI().init(scanner, tracker);
+	}
+}
