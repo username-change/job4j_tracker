9a710d2241d600aabfa1cabc5b55515967803514
Diletant <al@naun.com>
2024-11-26 16:22:58 +0700

    5. Tracker - хранилище [#396 #523400]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -32,14 +32,7 @@ public class Tracker {
 	}
 
 	public Item[] findAll() {
-		Item[] result = new Item[size];
-		int count = 0;
-		for (int index = 0; index < size; index++) {
-			if (items[index] != null) {
-				result[count++] = items[index];
-			}
-		}
-		return Arrays.copyOf(items, count);
+		return Arrays.copyOf(items, size);
 	}
 
 	private int indexOf(int id) {
