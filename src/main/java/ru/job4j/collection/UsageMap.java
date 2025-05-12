74937cbae1f0efe66b5aab82aace6bb77bf23fc3
alex <differentplayer@yandex.ru>
2025-05-12 12:55:29 +0700

    0. Map, HashMap [#212652 #542968]
    - добавленый дополнительные пары ключ-значение, включая дублирующий

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
 	public static void main(String[] args) {
 		HashMap<String, String> map = new HashMap<>();
 		map.put("parsentev@yandex.ru", "Petr Arsentev");
+		map.put("parsentev@yandex.ru", "Petr Arsentev");
+		map.put("ivanova@example.com", "Anna Ivanova");
+		map.put("smirnov@gmail.com", "Dmitri Smirnov");
 
 		for (String key : map.keySet()) {
 			String value = map.get(key);
