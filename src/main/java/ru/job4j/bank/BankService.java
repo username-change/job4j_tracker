06d747b731ee849a2f6c02d3034f9e4459f8160e
Alex <differentplayer@yandex.ru>
2025-05-13 14:39:30 +0700

    3. Банковские переводы [#10038 #543063]
    - оптимизирован метод deleteUser

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -6,65 +6,66 @@ import java.util.List;
 import java.util.Map;
 
 public class BankService {
-    private final Map<User, List<Account>> users = new HashMap<>();
+	private final Map<User, List<Account>> users = new HashMap<>();
 
-    public void addUser(User user) {
-        users.putIfAbsent(user, new ArrayList<>());
-    }
+	public void addUser(User user) {
+		users.putIfAbsent(user, new ArrayList<>());
+	}
 
-    public void deleteUser(String passport) {
-        User user = findByPassport(passport);
-        if (user != null && user.getPassport().equals(passport)) {
-            users.remove(user);
-        }
-    }
+	public void deleteUser(String passport) {
+		User user = findByPassport(passport);
+		if (user != null) {
+			users.remove(user);
+		}
+	}
 
-    public void addAccount(String passport, Account account) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            List<Account> accounts = users.get(user);
-            if (!accounts.contains(account)) {
-                accounts.add(account);
-            }
-        }
-    }
+	public void addAccount(String passport, Account account) {
+		User user = findByPassport(passport);
+		if (user != null) {
+			List<Account> accounts = users.get(user);
+			if (!accounts.contains(account)) {
+				accounts.add(account);
+			}
+		}
+	}
 
-    public User findByPassport(String passport) {
-        for (User user : users.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                return user;
-            }
-        }
-        return null;
-    }
+	public User findByPassport(String passport) {
+		for (User user : users.keySet()) {
+			if (user.getPassport().equals(passport)) {
+				return user;
+			}
+		}
+		return null;
+	}
 
-    public Account findByRequisite(String passport, String requisite) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            List<Account> accounts = users.get(user);
-            for (Account account : accounts) {
-                if (account.getRequisite().equals(requisite)) {
-                    return account;
-                }
-            }
+	public Account findByRequisite(String passport, String requisite) {
+		User user = findByPassport(passport);
+		if (user != null) {
+			List<Account> accounts = users.get(user);
+			for (Account account : accounts) {
+				if (account.getRequisite().equals(requisite)) {
+					return account;
+				}
+			}
 
-        }
-        return null;
-    }
+		}
+		return null;
+	}
 
-    public boolean transferMoney(String sourcePassport, String sourceRequisite, String destinationPassport, String destinationRequisite, double amount) {
-        Account sourceAccount = findByRequisite(sourcePassport, sourceRequisite);
-        Account destinationAccount = findByRequisite(destinationPassport, destinationRequisite);
+	public boolean transferMoney(String sourcePassport, String sourceRequisite, String destinationPassport,
+			String destinationRequisite, double amount) {
+		Account sourceAccount = findByRequisite(sourcePassport, sourceRequisite);
+		Account destinationAccount = findByRequisite(destinationPassport, destinationRequisite);
 
-        if (sourceAccount != null && destinationAccount != null && sourceAccount.getBalance() >= amount) {
-            sourceAccount.setBalance(sourceAccount.getBalance() - amount);
-            destinationAccount.setBalance(destinationAccount.getBalance() + amount);
-            return true;
-        }
-        return false;
-    }
+		if (sourceAccount != null && destinationAccount != null && sourceAccount.getBalance() >= amount) {
+			sourceAccount.setBalance(sourceAccount.getBalance() - amount);
+			destinationAccount.setBalance(destinationAccount.getBalance() + amount);
+			return true;
+		}
+		return false;
+	}
 
-    public List<Account> getAccounts(User user) {
-        return users.get(user);
-    }
+	public List<Account> getAccounts(User user) {
+		return users.get(user);
+	}
 }
