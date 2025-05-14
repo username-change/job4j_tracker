a244bac020931f4e8d603015b5c74e9d507f78b1
alex <differentplayer@yandex.ru>
2025-05-14 14:02:45 +0700

    0. Сортировка [#10096 #543224]

--- src/main/java/ru/job4j/tracker/ItemDescByName.java
+++ src/main/java/ru/job4j/tracker/ItemDescByName.java
@@ -1,12 +1,11 @@
 package ru.job4j.tracker;
 
-import java.util.Collections;
 import java.util.Comparator;
 
 public class ItemDescByName implements Comparator<Item> {
 	@Override
 	public int compare(Item first, Item second) {
-		return Collections.reverseOrder().compare(first.getName(), second.getName());
+		return second.getName().compareTo(first.getName());
 	}
 
 }
