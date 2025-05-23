18b46fa2f9519173345a8cc33ba260cd1195d000
alex <differentplayer@yandex.ru>
2025-05-23 22:19:45 +0700

    17. Путь от циклов к Stream API. [#24260]

---
+++ src/main/java/ru/job4j/stream/Account.java
@@ -0,0 +1,5 @@
+package ru.job4j.stream;
+
+public record Account(String requisite, double balance) {
+
+}
---
+++ src/main/java/ru/job4j/stream/User.java
@@ -0,0 +1,5 @@
+package ru.job4j.stream;
+
+public record User(String passport, String username) {
+
+}
