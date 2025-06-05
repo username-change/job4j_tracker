b67501ddb3577606d0ae5716d909927d5835528c
alex <differentplayer@yandex.ru>
2025-06-06 00:40:57 +0700

    3. Аттестация [#49777 #545399]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -30,7 +31,9 @@ public class Analyze {
 		return stream
 				.flatMap(pupil -> pupil.subjects().stream())
 				.collect(Collectors.groupingBy(
-						Subject::name, Collectors.averagingDouble(Subject::score)
+						Subject::name,
+						LinkedHashMap::new,
+						Collectors.averagingDouble(Subject::score)
 				))
 				.entrySet()
 				.stream()
